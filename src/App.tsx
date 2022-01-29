d7f2279d1cde477f4aaddd281f7771485bb0b985
sangminlee98 <tkdals0975@naver.com>
2022-01-30 00:43:27 +0900

    use todo state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
-function App() {
+export type Todos = {
+  id: number,
+  text: string,
+  checked: boolean
+};
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todos[]>([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert/>
-      <TodoList/>
+      <TodoList todos={todos}/>
     </TodoTemplate>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Todos } from '../App';
 import './TodoList.scss'
 import TodoListItem from './TodoListItem';
 
-const TodoList = () => {
+const TodoList = (props: {todos: Todos[]}) => {
   return (
     <div className='TodoList'>
-      <TodoListItem/>
-      <TodoListItem/>
-      <TodoListItem/>
+      {
+        props.todos.map((todo: { id: number, text: string, checked: boolean }) => (
+          <TodoListItem todo={todo} key={todo.id}/>
+        ))
+      }
     </div>
   );
 };
